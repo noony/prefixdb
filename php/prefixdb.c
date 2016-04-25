ca64081ae7d6758ca701aee3d38f27305f32adc0
Thomas Colomb <thomas.colomb@dailymotion.com>
2016-04-25 21:46:10 +0000

    PHP7 Fixes

--- php/prefixdb.c
+++ php/prefixdb.c
@@ -35,34 +35,35 @@ static zend_object_handlers prefixdb_object_handlers;
 static inline PREFIXDB_OBJECT *php_prefixdb_fetch_object(zend_object *obj) {
     return (PREFIXDB_OBJECT *)((char *)obj - XtOffsetOf(PREFIXDB_OBJECT, zo));
 }
-#define Z_PREFIXDB_OBJ_P(zv) php_prefixdb_fetch_object(Z_OBJ_P(zv));
+#define Z_PREFIXDB_OBJ_P(zv) php_prefixdb_fetch_object(Z_OBJ_P(getThis()));
 
 khash_t(PREFIXDB_HASH) *prefixdb_cache;
 zend_class_entry *prefixdb_class_entry;
 
 PHP_METHOD(PrefixDB, __construct)
 {
-    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P(getThis());
+    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P();
     PREFIXDB_CACHE  *cache;
     PREFIXDB        *pfdb;
     khiter_t        khit;
     struct stat     info;
     time_t          now;
-    zend_string     *path;
+    char            *path = NULL;
+    size_t          length = 0;
     int             status;
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|S", &path) == SUCCESS && (path && path->len))
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|s", &path, &length) == SUCCESS && length)
     {
-        if ((khit = kh_get(PREFIXDB_HASH, prefixdb_cache, path->val)) != kh_end(prefixdb_cache))
+        if ((khit = kh_get(PREFIXDB_HASH, prefixdb_cache, path)) != kh_end(prefixdb_cache))
         {
             cache = kh_value(prefixdb_cache, khit);
             time(&now);
             if (now - cache->checked >= 30)
             {
                 cache->checked = now;
-                if (!stat(path->val, &info) && info.st_mtime > cache->modified && info.st_mtime <= (now - 5))
+                if (!stat(path, &info) && info.st_mtime > cache->modified && info.st_mtime <= (now - 5))
                 {
-                    if ((pfdb = prefixdb_load_file(path->val, 0)))
+                    if ((pfdb = prefixdb_load_file(path, 0)))
                     {
                         prefixdb_free(&(cache->db));
                         cache->db       = pfdb;
@@ -70,7 +71,7 @@ PHP_METHOD(PrefixDB, __construct)
                     }
                     else
                     {
-                        php_error_docref(NULL TSRMLS_CC, E_WARNING, "cannot open or invalid PrefixDB database \"%s\"", path->val);
+                        php_error_docref(NULL TSRMLS_CC, E_WARNING, "cannot open or invalid PrefixDB database \"%s\"", path);
                     }
                 }
             }
@@ -79,9 +80,9 @@ PHP_METHOD(PrefixDB, __construct)
         }
         else
         {
-            if (!(instance->db = prefixdb_load_file(path->val, 0)))
+            if (!(instance->db = prefixdb_load_file(path, 0)))
             {
-                php_error_docref(NULL TSRMLS_CC, E_WARNING, "cannot open or invalid PrefixDB database \"%s\"", path->val);
+                php_error_docref(NULL TSRMLS_CC, E_WARNING, "cannot open or invalid PrefixDB database \"%s\"", path);
             }
             else if ((cache = (PREFIXDB_CACHE *)calloc(1, sizeof(PREFIXDB_CACHE))))
             {
@@ -89,11 +90,11 @@ PHP_METHOD(PrefixDB, __construct)
                 cache->modified  = cache->checked;
                 cache->db        = instance->db;
                 instance->cached = 1;
-                if (!stat(path->val, &info))
+                if (!stat(path, &info))
                 {
                     cache->modified = info.st_mtime;
                 }
-                khit = kh_put(PREFIXDB_HASH, prefixdb_cache, strdup(path->val), &status);
+                khit = kh_put(PREFIXDB_HASH, prefixdb_cache, strdup(path), &status);
                 kh_value(prefixdb_cache, khit) = cache;
             }
         }
@@ -106,39 +107,39 @@ PHP_METHOD(PrefixDB, __construct)
 
 PHP_METHOD(PrefixDB, add)
 {
-    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P(getThis());
-    // PREFIXDB_OBJECT *instance = (PREFIXDB_OBJECT *)zend_object_store_get_object(getThis() TSRMLS_CC);
-    zend_string     *prefix;
+    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P();
+    char            *prefix = NULL;
+    size_t          length = 0;
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "S", &prefix) == SUCCESS && prefix->len && !instance->cached)
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &prefix, &length) == SUCCESS && length && !instance->cached)
     {
-        RETURN_LONG(prefixdb_add_string(instance->db, prefix->val, NULL));
+        RETURN_LONG(prefixdb_add_string(instance->db, prefix, NULL));
     }
     RETURN_LONG(PREFIXDB_ERROR_PARAM);
 }
 
 PHP_METHOD(PrefixDB, save)
 {
-    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P(getThis());
-    // PREFIXDB_OBJECT *instance = (PREFIXDB_OBJECT *)zend_object_store_get_object(getThis() TSRMLS_CC);
-    zend_string     *path;
+    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P();
+    char                *path = NULL;
+    size_t             length = 0;
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "S", &path) == SUCCESS && path->len)
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &path, &length) == SUCCESS && length)
     {
-        RETURN_LONG(prefixdb_save_file(instance->db, path->val));
+        RETURN_LONG(prefixdb_save_file(instance->db, path));
     }
     RETURN_LONG(PREFIXDB_ERROR_PARAM);
 }
 
 PHP_METHOD(PrefixDB, search)
 {
-    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P(getThis());
-    // PREFIXDB_OBJECT *instance = (PREFIXDB_OBJECT *)zend_object_store_get_object(getThis() TSRMLS_CC);
-    zend_string     *address;
+    PREFIXDB_OBJECT *instance = Z_PREFIXDB_OBJ_P();
+    char             *address = NULL;
+    size_t             length = 0;
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "S", &address) == SUCCESS && address->len)
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &address, &length) == SUCCESS && length)
     {
-        if (prefixdb_search_string(instance->db, address->val, NULL) == PREFIXDB_ERROR_OK)
+        if (prefixdb_search_string(instance->db, address, NULL) == PREFIXDB_ERROR_OK)
         {
             RETURN_STRING("{}");
         }
@@ -148,23 +149,24 @@ PHP_METHOD(PrefixDB, search)
 
 static void prefixdb_dtor(zend_object *obj)
 {
-    PREFIXDB_OBJECT *i_obj = (PREFIXDB_OBJECT *)((char *)obj - XtOffsetOf(PREFIXDB_OBJECT, zo));
-    zend_object_std_dtor(&i_obj->zo);
+    PREFIXDB_OBJECT *i_obj = php_prefixdb_fetch_object(obj);
     if (!i_obj->cached)
     {
         prefixdb_free(&(i_obj->db));
     }
+    zend_object_std_dtor(&i_obj->zo);
     i_obj = NULL;
+    obj = NULL;
     efree(i_obj);
+    efree(obj);
 }
 
 static zend_object* prefixdb_ctor(zend_class_entry *ce TSRMLS_DC)
 {
     PREFIXDB_OBJECT *instance;
-
     instance = ecalloc(1, sizeof(PREFIXDB_OBJECT));
     zend_object_std_init(&instance->zo, ce);
-    instance->zo.handlers = zend_get_std_object_handlers();
+    instance->zo.handlers = &prefixdb_object_handlers;
 
     return &instance->zo;
 }
@@ -188,14 +190,18 @@ PHP_MINIT_FUNCTION(prefixdb)
     zend_class_entry ce;
 
     memcpy(&prefixdb_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+
+    INIT_CLASS_ENTRY(ce, "PrefixDB", prefixdb_class_methods);
+    ce.create_object = prefixdb_ctor;
+
     prefixdb_object_handlers.offset = XtOffsetOf(PREFIXDB_OBJECT, zo);
     prefixdb_object_handlers.clone_obj = NULL;
     prefixdb_object_handlers.free_obj = prefixdb_dtor;
 
-    INIT_CLASS_ENTRY(ce, "PrefixDB", prefixdb_class_methods);
     prefixdb_class_entry = zend_register_internal_class(&ce);
-    prefixdb_class_entry->create_object = prefixdb_ctor;
+
     prefixdb_cache = kh_init(PREFIXDB_HASH);
+
     REGISTER_LONG_CONSTANT("PREFIXDB_ERROR_OK",       PREFIXDB_ERROR_OK,       CONST_CS | CONST_PERSISTENT);
     REGISTER_LONG_CONSTANT("PREFIXDB_ERROR_PARAM",    PREFIXDB_ERROR_PARAM,    CONST_CS | CONST_PERSISTENT);
     REGISTER_LONG_CONSTANT("PREFIXDB_ERROR_MEMORY",   PREFIXDB_ERROR_MEMORY,   CONST_CS | CONST_PERSISTENT);
